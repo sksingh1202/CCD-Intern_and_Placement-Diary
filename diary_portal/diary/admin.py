b40530ab2e787c09804e77a13245f6dbabf5a40e
Saket Kumar Singh <saketkumar1202@gmail.com>
2020-08-07 16:30:20 +0530

    Added User model to the admin page

--- diary_portal/diary/admin.py
+++ diary_portal/diary/admin.py
@@ -3,6 +3,7 @@ from . import models
 
 # Register your models here.
 
+admin.site.register(models.User)
 admin.site.register(models.Company)
 admin.site.register(models.HR)
 admin.site.register(models.Remark)
