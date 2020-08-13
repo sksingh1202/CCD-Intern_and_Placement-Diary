9294ada2df445bcc65099122cd3edef62c634bef
chelsi-001011 <57683338+chelsi-001011@users.noreply.github.com>
2020-08-13 18:23:28 +0530

    Added Detail view of models in Admin

--- diary_portal/diary/admin.py
+++ diary_portal/diary/admin.py
@@ -4,7 +4,30 @@ from . import models
 
 # Register your models here.
 
+class CompanyAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+    list_filter = ['POC','placement','internship']
+
+    list_display = ['name','POC','additional_POC','CPOC','placement','internship']
+
+    list_editable = ['POC','additional_POC','CPOC']
+
+class   HRAdmin(admin.ModelAdmin):
+    search_fields = ['company']
+    list_filter = ['company']
+    list_display =['name','company','contact_number_1','contact_number_2','email','linkedin_id','facebook_id']
+
+
+class RemarkAdmin(admin.ModelAdmin):
+    search_fields = ['company','user']
+    list_filter = ['company','user']
+    list_display = ['remark','company','user','datetime','placement']
+    # list_display_links = ['remark']
+    # list_editable_links = ['remark']
+
+
 admin.site.register(models.User, UserAdmin)
-admin.site.register(models.Company)
-admin.site.register(models.HR)
-admin.site.register(models.Remark)
+admin.site.register(models.Company,CompanyAdmin)
+admin.site.register(models.HR,HRAdmin)
+admin.site.register(models.Remark,RemarkAdmin)
