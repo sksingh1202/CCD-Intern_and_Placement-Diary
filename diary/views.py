77b21898f924412d95854f689a4dc4e3467ab0d2
sksingh1202 <59494843+sksingh1202@users.noreply.github.com>
2021-04-06 20:49:53 +0530

    Update views.py

--- diary/views.py
+++ diary/views.py
@@ -41,7 +41,7 @@ def company_update(request, *args, **kwargs):
     global GLOBAL_YR
     context = {'company_list':models.Company.objects.filter(datetime__year=GLOBAL_YR), 'yr':GLOBAL_YR}
     if request.method == "POST" and len(str(request.POST.get('search_text'))):
-        context['company_list'] = models.Company.objects.filter(datetime_year=GLOBAL_YR, name_icontains=request.POST.get("search_text"))
+        context['company_list'] = models.Company.objects.filter(datetime__year=GLOBAL_YR, name__icontains=request.POST.get("search_text"))
     return render(request, "diary/_filtered_companies.html", context)
 
 def company_intern_placement_filter(request, *args, **kwargs):
